478d09378dca1bce5b0db696a62c78e9a12c2a03
André Filipe Pinto Vieira <andvieira.16@gmail.com>
2022-07-19 00:59:51 +0100

    fix issue

--- src/css/monaco.contribution.ts
+++ src/css/monaco.contribution.ts
@@ -81,15 +81,14 @@ export interface ModeConfiguration {
 	readonly foldingRanges?: boolean;
 
 	/**
-	 * Defines diagnostic codes to be ignored.
-	 */
-	 readonly diagnosticCodesToIgnore?: number[];
-
-	/**
 	 * Defines whether the built-in diagnostic provider is enabled.
 	 */
 	readonly diagnostics?: boolean;
 
+	/**
+	 * Defines diagnostic codes to be ignored.
+	 */
+	readonly diagnosticCodesToIgnore?: number[];
 
 	/**
 	 * Defines whether the built-in selection range provider is enabled.
